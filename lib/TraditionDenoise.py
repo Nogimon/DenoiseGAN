161780a548f3892eaf604535bded339e34ede7a5
Nogimon <x3722p@gmail.com>
2018-10-09 00:30:55 -0400

    start skimage

--- lib/TraditionDenoise.py
+++ lib/TraditionDenoise.py
@@ -3,29 +3,39 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from Skimage import denoise_tv_chambolle
 
 class DenoiseCv2:
 
-    def __init__(directory):
+
+
+    def __init__(self, directory):
         self.directory = directory
 
-    def plotImage(img, name):
+    def plotImage(self, img, name):
         plt.savefig("./" + name + ".png")
 
 
 
-    def denoiseFastNIMeans():
-        img = cv2.read(self.directory)
+    def denoiseFastNIMeans(self):
+        img = cv2.imread(self.directory)
         denoisedImg = cv2.fastNIMeansDenoisingColored(img, None, 10, 10, 7, 21)
 
         self.plotImage(img, "NIMeans")
 
 
+class Skimage:
+
+    def __init__(self, directory):
+        self.directory = directory
+
+    def denoiseTVC(self):
+        img = 
 
 
 
 if __name__ == "__main__":
-    dir = ""
-    denoiseCv2 = DenoiseCv2()
+    dir = "./try.png"
+    denoiseCv2 = DenoiseCv2(dir)
     denoiseCv2.denoiseFastNIMeans()
 
